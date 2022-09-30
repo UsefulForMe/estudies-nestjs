00043f892bbcfeae4dcc679dc18cb4dee06ea3d3
trongdev <hiiamtrong@gmail.com>
2022-09-30 23:50:56 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppLoggerService } from 'src/logger/logger.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
